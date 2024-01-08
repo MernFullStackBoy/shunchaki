3b580c75c4cf83bcb21d2df3b455315b64816bf4
MernFullStackBoy <jahongirmurtozayev777@gmail.com>
2024-01-08 10:46:26 +0500

    shunchki

---
+++ script.js
@@ -0,0 +1,29 @@
+// let str = "String"
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+// let arr = ["String", 15, true, 58n, [15], { name: "Name" }]
+// for (let a in arr) {
+//     console.log(a);
+// }
+
+// let arr = ["String", 15, true, 58n, [15], { name: "Name" }]
+// for (let a of arr) {
+//     console.log(a);i
+// }
+// arr.forEach((item) => {
+//     if (typeof item == 'string') {
+//         item.toUpperCase()
+//     }
+// })
+
+// let i = 0
+// while (i < 10) {
+//     console.log(i++);
+// }
+// let i = 0
+// do {
+//     console.log(++i);
+// } while (i < 20)
+
